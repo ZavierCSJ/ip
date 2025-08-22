58244141484628269949ba68dcab0579c8d86ef5
ZavierCSJ <zavierchew@gmail.com>
2025-08-22 14:14:45 +0800

    Complete level 1. Echo

--- src/main/java/Teemo.java
+++ src/main/java/Teemo.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Teemo {
     public static void main(String[] args) {
+        int x = 1;
         System.out.println("____________________________________");
         System.out.println("Hello! I'm Teemo");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________");
+        while (x != 0) {
+            Scanner scanner = new Scanner(System.in);
+            String text = scanner.nextLine();
+            if (text.equals("bye")) {
+                x = 0;
+                System.out.println("____________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________");
+                scanner.close();
+            } else {
+                System.out.println("____________________________________");
+                System.out.println(text);
+                System.out.println("____________________________________");
+            }
+        }
     }
 }
