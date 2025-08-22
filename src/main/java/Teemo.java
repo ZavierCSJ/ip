0fc224721b3c3a64f5794ea8dac943a4f17288aa
ZavierCSJ <zavierchew@gmail.com>
2025-08-22 13:39:48 +0800

    Add greeting and change name

---
+++ src/main/java/Teemo.java
@@ -0,0 +1,10 @@
+public class Teemo {
+    public static void main(String[] args) {
+        System.out.println("____________________________________");
+        System.out.println("Hello! I'm Teemo");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________");
+    }
+}
