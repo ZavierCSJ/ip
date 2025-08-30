7efaa1b50d20eebf4900e25979c949019a0af039
ZavierCSJ <zavierchew@gmail.com>
2025-08-30 14:00:24 +0800

    A-MoreOOP: Create Ui class

---
+++ src/main/java/teemo/Ui.java
@@ -0,0 +1,86 @@
+package teemo;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import teemo.task.Task;
+
+public class Ui {
+    private Scanner scanner;
+
+    public Ui() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void showWelcome() {
+        showLine();
+        System.out.println("Hello! I'm Teemo");
+        System.out.println("What can I do for you?");
+        showLine();
+    }
+
+    public void showBye() {
+        System.out.println("____________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________");
+    }
+
+    public void showLine() {
+        System.out.println("____________________________________");
+    }
+
+    public String readCommand() {
+        return scanner.nextLine();
+    }
+
+    public void showTaskList(ArrayList<Task> tasks) {
+        showLine();
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.printf("%d.%s\n", i + 1, tasks.get(i).toString());
+        }
+        showLine();
+    }
+
+    public void showTaskAdded(Task task, int totalTasks) {
+        showLine();
+        System.out.println("Got it. I've added this task:");
+        System.out.println("" + task);
+        System.out.printf("Now you have %d tasks in the list.\n", totalTasks);
+        showLine();
+    }
+
+    public void showTaskMarked(Task task) {
+        showLine();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(task.toString());
+        showLine();
+    }
+
+    public void showTaskUnmarked(Task task) {
+        showLine();
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println(task.toString());
+        showLine();
+    }
+
+    public void showTaskDeleted(Task task) {
+        showLine();
+        System.out.println("Noted. I've removed this task:");
+        System.out.println(task.toString());
+        showLine();
+    }
+
+    public void showError(String message) {
+        showLine();
+        System.out.println(message);
+        showLine();
+    }
+
+    public void showLoadingError() {
+        System.out.println("Error loading tasks from file. Starting with empty task list.");
+    }
+
+    public void close() {
+        scanner.close();
+    }
+}
